4a3a98a2f435016ee92c73362e6d605273f7927c
sortifal <sortifal@gmail.com>
2022-06-08 21:58:16 +0200

    test commit

--- minispot.h
+++ minispot.h
@@ -1,23 +1,24 @@
 #ifndef MINISPOT_H
 #define MINISPOT_H
 
+#define DEBUG 0
+
 #define PWM_SERVO_ADDR 0x40
 
-namespace SPOT{
-
-typdef struct spot{
-
-    //Position
-    int pos_x;
-    int pos_y;
-    int pos_z;
-    //Angles
-    int angle_x;
-    int angle_y;
-    int angle_z;
-
-}spot
-
-}
+class Spot{
+    private:
+        //Nothing here
+    public:
+        //Constructor
+        Spot(int x = 0, int y = 0, int z = 0);
+        //Position
+        int pos_x;
+        int pos_y;
+        int pos_z;
+        //Angles
+        int angle_x;
+        int angle_y;
+        int angle_z;
+};
 
 #endif 
