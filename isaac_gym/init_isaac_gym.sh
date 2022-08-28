9c2590c40f56f3ee40699efc9aa24c676c345e3d
Machin1234 <86405270+Machin1234@users.noreply.github.com>
2022-08-28 15:58:10 +0200

    truc en +
    truc en +

---
+++ isaac_gym/init_isaac_gym.sh
@@ -0,0 +1,12 @@
+cd
+cd Documents/GitHub/minispot/isaac_gym/isaac_gym/isaacgym/
+conda activate rlgpu
+export LD_LIBRARY_PATH=/home/machin/miniconda3/envs/rlgpu/lib:$LD_LIBRARY_PATH
+cd python/examples
+cd /home/machin/Documents/GitHub/minispot/isaac_gym/legged_gym_isaac/legged_gym/scripts/
+python train.py --task=anymal_c_flat
+
+
+
+
+#https://github.com/chengxuxin/legged_gym_isaac
